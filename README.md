dc43e58bd71f8b9eb5bfece40452b9af9441f4a3
akshayblevel <akshayblevel@gmail.com>
2021-09-04 07:45:12 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-ibrrdp
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-ibrrdp)
---
+++ src/app/employee-component/employeedetail-component/employeedetail-component.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './employeedetail-component.component.html',
+  styleUrls: ['./employeedetail-component.component.css']
+})
+export class EmployeedetailComponentComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
